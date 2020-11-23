5c9e6efb0eb1750147d6e9fc8604d9b15113ff35
Ronak Jethwa <ronakjethwa@ronakjethwa.local>
2020-11-23 01:45:09 -0500

    roboto

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,7 +1,6 @@
 import React, { useState } from "react"
 import PropTypes from "prop-types"
 import styled, { ThemeProvider } from "styled-components"
-import "typeface-roboto"
 
 import Context from "../context/"
 import Theme from "../styles/Theme"
